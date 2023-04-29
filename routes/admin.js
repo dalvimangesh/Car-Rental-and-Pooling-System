4e8a6a6cb0b8ba6e659d1620a680ea77e070be6a
dalvimangesh <dalvimangesh000@gmail.com>
2023-04-30 00:51:26 +0530

    view car with ejs

--- routes/admin.js
+++ routes/admin.js
@@ -19,7 +19,7 @@ router.post('/', (req, res) => {
 
 router.get('/view_car', async (req, res) => {
 
-    q = `select * from car`
+    q = `select * from car natural join car_category natural join location`
     var data = null
 
     try {
@@ -40,17 +40,68 @@ router.get('/view_car', async (req, res) => {
 
     console.log(data)
 
-    res.render('view_car.ejs', { data : data })
+    res.render('view_car.ejs', { data: data })
 
 })
 
-router.post('/view_car', (req, res) => {
+router.post('/view_car', async (req, res, next) => {
 
     const form = req.body
 
+    console.log(form)
+
+    q = `select category_id from car_category where category_name = '${form["Car Category"]}'`
+    var categoryID = null
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            dbConnect.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+            );
+        });
+        categoryID = result.rows[0].category_id
+    } catch (err) {
+        next(err);
+    }
+
+    console.log(form)
+
+    q = `select location_id from location where location_name = '${form["Location"]}'`
+    var locationID = null
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            dbConnect.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+            );
+        });
+        locationID = result.rows[0].location_id
+    } catch (err) {
+        next(err);
+    }
+
+    var status = null
+
+    if( form['Availability'] == 'Yes' ) {
+        status = 1
+    }
+    else{
+        status = 0
+    }
+
     q = `insert into car(model_name , registration_no , capacity , mileage , availability , category_id , location_id) values 
     (
-        '${form['Model Name']}','${form['Registration Number']}', '${form['Capacity']}','${form['Mileage']}', 0 ,123,123
+        '${form['Model Name']}','${form['Registration Number']}', '${form['Capacity']}','${form['Mileage']}', ${status} , ${categoryID} , ${locationID}
     )`
 
     dbConnect.query(q, (err, result) => {
@@ -63,29 +114,74 @@ router.post('/view_car', (req, res) => {
 })
 
 
-router.get('/add_car', (req, res) => {
+router.get('/add_car', async (req, res) => {
 
-    res.render('add_car.ejs')
+
+    q = `select category_name from car_category`
+    var category_name = null
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            dbConnect.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+            );
+        });
+        category_name = result.rows
+    } catch (err) {
+        next(err);
+    }
+
+    q = `select location_name from location`
+    var location_name = null
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            dbConnect.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+            );
+        });
+        location_name = result.rows
+    } catch (err) {
+        next(err);
+    }
+
+    console.log(category_name)
+    console.log(location_name)
+
+    res.render('add_car.ejs', {
+        category_name: category_name,
+        location_name: location_name
+    })
 
 })
 
 
 // car category
 
-router.get('/view_car_category', async (req, res,next) => {
+router.get('/view_car_category', async (req, res, next) => {
 
     q = `select * from car_category`
     var data = null
 
     try {
         const result = await new Promise((resolve, reject) => {
-            dbConnect.query(q,(err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
+            dbConnect.query(q, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
                 }
+            }
             );
         });
         data = result.rows
@@ -95,14 +191,14 @@ router.get('/view_car_category', async (req, res,next) => {
 
     console.log(data)
 
-    res.render('view_car_category.ejs', { data : data })
+    res.render('view_car_category.ejs', { data: data })
 
 
 })
 router.post('/view_car_category', (req, res) => {
 
-     const form = req.body
-     console.log("here");
+    const form = req.body
+    console.log("here");
     console.log(form)
     q = `insert into car_category(category_name,cost_perday,latefee_perday,bootspace) values 
     (
