50fcb9a9eaed81c532127c2f4471b91d481b9364
NideeshN <112001028@smail.iitpkd.ac.in>
2023-05-01 12:42:15 +0530

    added member

--- routes/become_member.js
+++ routes/become_member.js
@@ -5,7 +5,9 @@ const router = express.Router()
 router.get('/',(req,res)=>{
 
     res.render('become_member.ejs',{
-        'Name' : req.session.username
+        'Name' : req.session.username,
+        isMember : req.session.member,
+        expiry_date : req.session.expiry_date
     })    
 
 })
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { dbConnect } = require("../data/database");
 
 const { localsName } = require('ejs');
+const { Collection } = require('mongodb');
 const router = express.Router()
 
 router.get('/', (req, res) => {
@@ -9,7 +10,8 @@ router.get('/', (req, res) => {
 
     res.render('home.ejs',{
         'Name' : req.session.username,
-        'isMember': req.session.member
+        isMember: req.session.member,
+        expiry_date : req.session.expiry_date
     })
 
 })
@@ -27,7 +29,7 @@ router.post('/',async (req,res,next)=>{
     var present = null
     var ismem = null 
     var expiry_date = null 
-    q2 = `select user_id,expiry_date from membership natural join customer_info natural join login_info where expiry_date >= now()::date` 
+    q2 = `select user_id,expiry_date from membership natural join login_info where expiry_date >= now()::date and user_name = '${form['User Name']}'` 
     try {
         const result = await new Promise((resolve, reject) => {
             dbConnect.query(q1, (err, result) => {
@@ -57,10 +59,8 @@ router.post('/',async (req,res,next)=>{
             );
         });
         ismem = result.rows.length;
-        if(ismem != null && ismem == 1){
+        if(ismem != null && ismem>0){
             expiry_date = result.rows[0].expiry_date
-            console.log(result.rows[0])
-            console.log(expiry_date.toISOString().slice(0, 10));
         }
         // console.log(exists);
     } catch (err) {
@@ -73,9 +73,15 @@ router.post('/',async (req,res,next)=>{
         req.session.login = null
         if(ismem!=null && ismem>0){
             req.session.member = 'Yes'
+            req.session.expiry_date = expiry_date
+            console.log("ofnwdnrng")
+            console.log(req.session.member)
+        }
+        else{
+            req.session.member = 'No'
         }
         req.session.username = req.body['User Name']
-    req.session.password = req.body['Password']
+        req.session.password = req.body['Password']
     res.redirect('/home')
     }
     else{
@@ -87,13 +93,51 @@ router.post('/',async (req,res,next)=>{
         console.log("trying to add member")
         console.log(form)
         console.log(req.session.username)
+        userid = 1 
+        // query to get the userid using user name
+        q1 = `select user_id from login_info where user_name = '${req.session.username}'`
+
+        try {
+            const result = await new Promise((resolve, reject) => {
+                dbConnect.query(q1, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                }
+                );
+            });
+            // console.log(result)
+            userid = result.rows[0].user_id
+        } catch (err) {
+            next(err);
+        }
+        
         // get the user_id using login_info table, calculate expiry date
+        
         q = `insert into membership(user_id,start_date,expiry_date) values(
-            1,now()::date,now()::date + ${form['months']}
+            ${userid},now()::date,now()::date + ${form['months']}
         )`
         try {
         const result = await new Promise((resolve, reject) => {
             dbConnect.query(q, (err, result) => {
+                if (err) { 
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            }
+            );
+        });
+    } catch (err) {
+        next(err);
+    }
+    // getting expiry date
+    q2 = `select user_id,expiry_date from membership natural join customer_info natural join login_info where expiry_date >= now()::date and user_name = '${req.session.username}'` 
+    try {
+        const result = await new Promise((resolve, reject) => {
+            dbConnect.query(q2, (err, result) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -102,10 +146,21 @@ router.post('/',async (req,res,next)=>{
             }
             );
         });
-        console.log("here");
+       expiry_date = result.rows[0].expiry_date.toISOString().slice(0, 10);
+        req.session.member= 'Yes'
+        req.session.expiry_date = expiry_date
+        
+        // console.log(exists);
     } catch (err) {
         next(err);
     }
+    console.log("Now")
+    console.log(expiry_date)
+    res.render('home.ejs',{
+        'Name': req.session.username,
+        isMember : 'Yes',
+        'expiry_date' : expiry_date
+    });
      }
     
     
